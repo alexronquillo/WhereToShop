89715109bef29997db10d9815c3a921552d2e18b
Alex Ronquillo <arr@Alexs-MacBook-Pro-2.local>
2015-02-13 16:21:04 -0500

    Moved project to Gradle

---
+++ src/main/java/com/wheretoshop/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.wheretoshop;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends ActionBarActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
