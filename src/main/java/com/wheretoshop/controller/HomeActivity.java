26e8c0b340602fa212c503d60cde317d01e76033
Alex Ronquillo <arr@Alexs-MacBook-Pro-2.local>
2015-02-13 21:03:10 -0500

    Added Product class

--- src/main/java/com/wheretoshop/controller/HomeActivity.java
+++ src/main/java/com/wheretoshop/controller/HomeActivity.java
@@ -3,6 +3,8 @@ package com.wheretoshop.controller;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
+import com.wheretoshop.R;
+
 public class HomeActivity extends ActionBarActivity
 {
     /** Called when the activity is first created. */
